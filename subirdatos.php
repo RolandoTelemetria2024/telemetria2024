fca0497c758e24995cdbc5dbdbd324ca36483798
RolandoTelemetria2024 <161094995+RolandoTelemetria2024@users.noreply.github.com>
2024-03-02 11:38:08 -0500

    Update subirdatos.php

--- subirdatos.php
+++ subirdatos.php
@@ -1,25 +1,19 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 require_once 'accesoBD.php';
-
-$objectResponse = new stdClass();
+$objectResponse= new stdClass();
 date_default_timezone_set("UTC");
 
-if (isset($_POST['value']) && isset($_POST['type'])) {
-    $value = $_POST['value'];
-    $type = $_POST['type'];
+if (isset($_POST['valor']) && isset($_POST['tipo']) && isset($_POST['id_device'])){ // aqui se agrego isset($_POST['id_device']
+    $valor = mysqli_real_escape_string(conexion::conectar(), $_POST['valor']);
+    $tipo = mysqli_real_escape_string(conexion::conectar(), $_POST['tipo']);
+    $id_device = mysqli_real_escape_string(conexion::conectar(), $_POST['id_device']); // aqui se agrego isset($_POST['id_device']
 
-    // Crear una instancia de accesoBD
-    $accesoBD = new accesoBD();
-
-    // Escapar las variables para prevenir inyección SQL (o preferiblemente, usar consultas preparadas)
-    $value = mysqli_real_escape_string(conexion::conectar(), $value);
-    $type = mysqli_real_escape_string(conexion::conectar(), $type);
-
-    $objectResponse = $accesoBD->subirDatos($value, $type);
-    echo json_encode($objectResponse);
-} else {
-    $objectResponse->code = '500';
-    echo json_encode($objectResponse);
+    // Llama al método subirDatos de la clase accesoBD, incluyendo el id_device
+    $objectResponse = accesoBD::subirDatos($valor, $tipo, $id_device);// aqui se agrego $id_device
+    echo(json_encode($objectResponse));
+}else{
+    $objectResponse->code = '500'; // Código de error si faltan variables
+    echo (json_encode($objectResponse));
 }
 ?>
